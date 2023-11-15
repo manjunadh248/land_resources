32b72e99afcfc4e62bc2f247f06e16780edb9af3
manjunadh248 <150153046+manjunadh248@users.noreply.github.com>
2023-11-15 21:51:23 +0530

    Files Uploaded

---
+++ script.js
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Land Resources</title>
+    <link href="style.css"  rel="stylesheet">
+   
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">Land Resources</div>
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#service-section">Services</a></li>
+                <li><a href="#resources">Resources</a></li>
+                <li><a href="aboutus.html">About Us</a></li>
+                <li><a href="contact us.html">Contact Us</a></li>
+            </ul>
+        </div>
+    </header>
+<head>
+    <title>Land Resources</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+            background-image: url(https://your-land-resources-background-image.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+        header {
+            background-color: #4CAF50; /* Green color */
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            font-size: 24px;
+        }
+        h2 {
+            font-size: 20px;
+        }
+        ul {
+            list-style-type: disc;
+            margin-left: 20px;
+        }
+        li {
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+        a {
+            text-decoration: none;
+            color: #00796b; /* Teal color */
+        }
+    </style>
+</head>
+<body>
+    
+    <div class="container">
+        <h2>Resources</h2>
+        <ul>
+            <li><a href="land-resource1.pdf" target="_blank">Resource 1: Land Management Guidelines</a></li>
+            <li><a href="land-resource2.pdf" target="_blank">Resource 2: Sustainable Land Development Practices</a></li>
+            <li><a href="land-resource3.pdf" target="_blank">Resource 3: Ecological Land Conservation Handbook</a></li>
+            <li><a href="https://www.example.com/land-article1" target="_blank">Article: Importance of Land Resources in Sustainable Development</a></li>
+            <li><a href="https://www.example.com/land-video1" target="_blank">Video: Land Planning and Development Best Practices</a></li>
+        </ul>
+    </div>
+</body>
+</html>
